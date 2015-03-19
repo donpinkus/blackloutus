b7306f5818e5d8b25ce0fd1f540233ad81ea8e61
Don Pinkus <don.pinkus@gmail.com>
2015-03-19 03:14:08 -0700

    improved decks#index

--- app/assets/javascripts/controllers/decks_index_controller.js
+++ app/assets/javascripts/controllers/decks_index_controller.js
@@ -1,11 +1,26 @@
 myApp.controller('decksIndex', ['$scope', '$http', '$routeParams', 'localStorageService', 'PlainsWalker', 'Deck', function($scope, $http, $routeParams, localStorageService, PlainsWalker, Deck){
   $scope.decks = localStorageService.get('decks');
-	console.log('hi!');
-
 
   // Add meta info to decks
 
   $scope.decks.forEach(function(deck, i, decks){
+    // Mana Proportions
+    var manaCounts = _.countBy(deck.cards, function(card){
+      return card.colors[0];
+    });
 
+    deck.manaProportions = [];
+
+    _.map(manaCounts, function(count, color){
+      deck.manaProportions[color] = Math.round((count / deck.cards.length) * 100);
+    });
+
+    // Max mana card
+    var maxCard = _.max(deck.cards, function(card){
+      return card.converted_mana_cost;
+    });
+
+    deck.maxCardId = maxCard.multiverse_id;
+    console.log(deck.maxCardId);
   });
 }]);
