8f08ab1ffe73722e054fa956ee2dfe32f093ed23
Don Pinkus <don.pinkus@gmail.com>
2015-03-23 00:11:26 -0700

    validate unique deck names

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,4 +2,7 @@ class Deck < ActiveRecord::Base
   belongs_to :plains_walker
   has_many :deck_cards
   has_many :cards, through: :deck_cards
+
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :plains_walker_id, presence: true
 end
