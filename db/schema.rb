69a5583f858729f642859ec77e5abc7737241970
louis-schiff <louis@vsco.co>
2015-03-23 21:22:24 -0700

    add last_used to Decks model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318071703) do
+ActiveRecord::Schema.define(version: 20150324042050) do
 
   create_table "cards", force: true do |t|
     t.integer  "multiverse_id"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20150318071703) do
     t.integer  "plains_walker_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "last_used"
   end
 
   add_index "decks", ["plains_walker_id"], name: "index_decks_on_plains_walker_id"
